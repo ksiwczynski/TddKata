7cbc1297ecd89d96e10bdfab376fa66b5b5f513b
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-26 09:26:19 +0200

    slajd 1

---
+++ TddKata/TddKata.Tests/StringCalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddKata.Tests
+{
+	public class StringCalculatorTest
+	{
+		[Test]
+		public void AddEmptyStringTest()
+		{
+			var calc = new StringCalculator();
+			Assert.That(calc.Add(""), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void AddSingleNumberTest()
+		{
+			var calc = new StringCalculator();
+			Assert.That(calc.Add("1"), Is.EqualTo(1));
+		}
+
+		[Test]
+		public void AddTwoNumbersTest()
+		{
+			var calc = new StringCalculator();
+			Assert.That(calc.Add("1,2"), Is.EqualTo(3));
+		}
+	}
+}
---
+++ TddKata/TddKata/StringCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddKata
+{
+	public class StringCalculator
+	{
+		public int Add(string v)
+		{
+			if (String.IsNullOrWhiteSpace(v))
+			{
+				return 0;
+			}
+
+			string[] vals = v.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+			if (vals.Length == 1)
+			{
+				return Int32.Parse(vals[0]);
+			}
+
+			return Int32.Parse(vals[0]) + Int32.Parse(vals[1]);
+		}
+	}
+}
